a82845d03af43de38822dc7dc65531abb2e604a6
liuaoz <576729125@qq.com>
2018-06-12 22:32:30 +0800

    x

--- src/main/java/com/liuaoz/juc/controller/concurrency/aqs/CyclicBarrierDemo1.java
+++ src/main/java/com/liuaoz/juc/controller/concurrency/aqs/CyclicBarrierDemo1.java
@@ -1,23 +1,47 @@
 package com.liuaoz.juc.controller.concurrency.aqs;
 
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
 
 /**
+ * CyclicBarrier
  * Created by matrix_stone on 2018/6/12.
  */
+@Slf4j
 public class CyclicBarrierDemo1 {
 
-    private static final int threadTotal = 10;
+    private static final int threadTotal = 5;
 
-    public static void main(String[] args) {
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(threadTotal);
+
+    public static void main(String[] args) throws Exception{
 
         ExecutorService executorService = Executors.newCachedThreadPool();
 
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(1, () -> {
+        for(int i = 0;i<10;i++){
+            Thread.sleep(1000);
+            final int threadNum = i ;
+            executorService.execute(()->{
+                race(threadNum);
+            });
+        }
 
-        });
+        executorService.shutdown();
+    }
 
+    private static void race(int threadNum){
+
+        try {
+
+            Thread.sleep(1000);
+            log.info("{} is ready.",threadNum);
+            cyclicBarrier.await(2000, TimeUnit.MILLISECONDS);
+
+            log.info("{} continue",threadNum);
+
+        } catch (Exception e) {
+            log.error("Exception:");
+        }
     }
 }
